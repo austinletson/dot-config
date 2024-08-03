e85038befa1c3e5412cdec505bbbe0c25304b6fd
austinletson <waustinletson@gmail.com>
2024-08-03 07:21:12 -0400

    tmux zsh and nvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Add any additional autocmds hereby
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown", "yaml" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
