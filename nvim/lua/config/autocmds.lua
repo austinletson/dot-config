f94be6396f45e268d316e93bef81d5c65f6ca2fb
austinletson <waustinletson@gmail.com>
2024-08-05 08:05:14 -0400

    tmux fzf

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds hereby
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "markdown", "yaml" },
+  pattern = { "yaml" },
   callback = function()
     vim.b.autoformat = false
   end,
