6e1b8851f60064d2e1ae90874f416cbdca8cf276
Austin Letson <austin.letson@axiomatic-ai.com>
2025-10-12 14:38:44 +0200

    code companion

--- astrovim/lua/plugins/code_companion.lua
+++ astrovim/lua/plugins/code_companion.lua
@@ -1,7 +1,14 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    opts = {
+      strategies = {
+        chat = {
+          adapter = 'anthropic',
+          model = 'claude-sonnet-4-20250514',
+        },
+      },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
