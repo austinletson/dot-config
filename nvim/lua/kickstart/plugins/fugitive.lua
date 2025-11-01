00a5b505997527e617e6e419a07a316e0ec5ca13
Austin Letson <austin.letson@axiomatic-ai.com>
2025-11-01 06:51:10 -0400

    GBrowse

--- nvim/lua/kickstart/plugins/fugitive.lua
+++ nvim/lua/kickstart/plugins/fugitive.lua
@@ -3,7 +3,13 @@ return {
 
   lazy = false,
 
+  -- vim-rhubarb adds GitHub support for :GBrowse
+  dependencies = {
+    'tpope/vim-rhubarb',
+  },
+
   keys = {
     { '<leader>gh', '<cmd>0Gclog<cr>', desc = 'Git file history' },
+    { '<leader>gB', '<cmd>GBrowse<cr>', desc = 'Git browse on GitHub' },
   },
 }
