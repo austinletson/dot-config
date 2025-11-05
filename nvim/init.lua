df3e4905b26dc9cf665167b33d1ac16c3ba0d8e0
Austin Letson <austin.letson@axiomatic-ai.com>
2025-11-05 08:02:38 -0500

    wip: remove mason

--- nvim/init.lua
+++ nvim/init.lua
@@ -199,13 +199,6 @@ require('lazy').setup({
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- Mason must be loaded before its dependents so we need to set it up here.
-      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'mason-org/mason.nvim', opts = {} },
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
 
@@ -329,25 +322,17 @@ require('lazy').setup({
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      -- Setup each LSP server manually using the new vim.lsp.config API
+      for server_name, server_config in pairs(servers) do
+        -- Merge capabilities with any server-specific capabilities
+        server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
 
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
+        -- Configure the LSP server using the new API
+        vim.lsp.config[server_name] = server_config
+
+        -- Enable the LSP server for appropriate filetypes
+        vim.lsp.enable(server_name)
+      end
     end,
   },
 
--- nvim/lua/kickstart/plugins/debug.lua
+++ nvim/lua/kickstart/plugins/debug.lua
@@ -34,11 +34,6 @@ return {
   },
 
   config = function()
-    -- load mason-nvim-dap here, after all adapters have been setup
-    if LazyVim.has("mason-nvim-dap.nvim") then
-      require("mason-nvim-dap").setup(LazyVim.opts("mason-nvim-dap.nvim"))
-    end
-
     vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
     for name, sign in pairs(LazyVim.config.icons.dap) do
